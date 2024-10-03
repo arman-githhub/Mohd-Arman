02a56ed983b4ba85489273ad688811aded8c1f90
arman-githhub <mohammed.23cs075@sode-edu.in>
2024-10-03 09:55:14 +0530

    Add files via upload

---
+++ A5.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class  Password Generator {
+   //Method to generate a
+random password
+    public static string
+            generatePassword(int length){
+     //
+}
+    
+}
